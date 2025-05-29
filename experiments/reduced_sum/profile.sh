d12c3c9bbf4a0426e973f5c7044111788915eb19
Kolen Cheung <christian.kolen@gmail.com>
2025-05-29 19:38:05 +0100

    test more env var combinations

--- experiments/reduced_sum/profile.sh
+++ experiments/reduced_sum/profile.sh
@@ -2,7 +2,58 @@
 
 M=8192
 
-for NUM_THREADS in 1 14; do
+: > profile_${M}.log
+
+# baseline where no env vars are set
+# 0.5 GiB, 1 GiB, 2 GiB
+for K in 8192 16384 32768; do
+    for case in \
+        numpy \
+        numba \
+        numba_parallel \
+        numba_fori \
+        numba_fori_parallel \
+        jax \
+        jax_vmap \
+        jax_fori \
+        jax_scan; do
+        echo "=========== ${case} M=${M}, K=${K}, NUM_THREADS=-1 ==========="
+        command time -v ipython profile_${case}.py ${M} ${K} 2>&1 >> profile_${M}.log
+    done
+done
+
+# non-JAX case where env var is clear to set
+for NUM_THREADS in 1 2 4 8 10 14; do
+    export MKL_NUM_THREADS=${NUM_THREADS}
+    export MKL_DOMAIN_NUM_THREADS="MKL_BLAS=${NUM_THREADS}"
+    export MKL_DYNAMIC=FALSE
+
+    export OMP_NUM_THREADS=${NUM_THREADS}
+    export OMP_PLACES=threads
+    export OMP_PROC_BIND=spread
+    export OMP_DYNAMIC=FALSE
+
+    export NUMEXPR_NUM_THREADS=${NUM_THREADS}
+
+    export OPENBLAS_NUM_THREADS=${NUM_THREADS}
+
+    export NUMBA_NUM_THREADS=${NUM_THREADS}
+    # 0.5 GiB, 1 GiB, 2 GiB
+    for K in 8192 16384 32768; do
+        for case in \
+            numpy \
+            numba \
+            numba_parallel \
+            numba_fori \
+            numba_fori_parallel; do
+            echo "=========== ${case} M=${M}, K=${K}, NUM_THREADS=${NUM_THREADS} ==========="
+            command time -v ipython profile_${case}.py ${M} ${K} 2>&1 >> profile_${M}.log
+        done
+    done
+done
+
+# JAX cases where we tests multiple ways to set env var
+for NUM_THREADS in 1 2 4 8 10 14; do
     export MKL_NUM_THREADS=${NUM_THREADS}
     export MKL_DOMAIN_NUM_THREADS="MKL_BLAS=${NUM_THREADS}"
     export MKL_DYNAMIC=FALSE
@@ -19,23 +70,52 @@ for NUM_THREADS in 1 14; do
     export NUMBA_NUM_THREADS=${NUM_THREADS}
 
     export NPROC=${NUM_THREADS}
-    export XLA_FLAGS="--xla_cpu_multi_thread_eigen=true intra_op_parallelism_threads=${NUM_THREADS} --xla_force_host_platform_device_count=${NUM_THREADS}"
+    export JAX_NUM_CPU_DEVICES=1
     export TF_NUM_INTEROP_THREADS=1
     export TF_NUM_INTRAOP_THREADS=${NUM_THREADS}
     # 0.5 GiB, 1 GiB, 2 GiB
     for K in 8192 16384 32768; do
         for case in \
-            numpy \
-            numba \
-            numba_parallel \
-            numba_fori \
-            numba_fori_parallel \
             jax \
             jax_vmap \
             jax_fori \
             jax_scan; do
             echo "=========== ${case} M=${M}, K=${K}, NUM_THREADS=${NUM_THREADS} ==========="
-            command time -v ipython profile_${case}.py ${M} ${K}
+            command time -v ipython profile_${case}.py ${M} ${K} 2>&1 >> profile_${M}.log
         done
     done
-done 2>&1 | tee profile_${M}.log
+done
+
+# JAX cases where we tests multiple ways to set env var
+for NUM_THREADS in 1 2 4 8 10 14; do
+    export MKL_NUM_THREADS=${NUM_THREADS}
+    export MKL_DOMAIN_NUM_THREADS="MKL_BLAS=${NUM_THREADS}"
+    export MKL_DYNAMIC=FALSE
+
+    export OMP_NUM_THREADS=${NUM_THREADS}
+    export OMP_PLACES=threads
+    export OMP_PROC_BIND=spread
+    export OMP_DYNAMIC=FALSE
+
+    export NUMEXPR_NUM_THREADS=${NUM_THREADS}
+
+    export OPENBLAS_NUM_THREADS=${NUM_THREADS}
+
+    export NUMBA_NUM_THREADS=${NUM_THREADS}
+
+    export NPROC=${NUM_THREADS}
+    export JAX_NUM_CPU_DEVICES=${NUM_THREADS}
+    export TF_NUM_INTEROP_THREADS=1
+    export TF_NUM_INTRAOP_THREADS=${NUM_THREADS}
+    # 0.5 GiB, 1 GiB, 2 GiB
+    for K in 8192 16384 32768; do
+        for case in \
+            jax \
+            jax_vmap \
+            jax_fori \
+            jax_scan; do
+            echo "=========== ${case}_all_cpu_devices M=${M}, K=${K}, NUM_THREADS=${NUM_THREADS} ==========="
+            command time -v ipython profile_${case}.py ${M} ${K} 2>&1 >> profile_${M}.log
+        done
+    done
+done
