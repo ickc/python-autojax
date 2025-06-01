560b893e4b8d49179c57d028b502f4f445415de7
Kolen Cheung <christian.kolen@gmail.com>
2025-06-01 21:13:46 +0100

    Create sharding.py

---
+++ experiments/reduced_sum/sharding.py
@@ -0,0 +1,85 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.1
+#   kernelspec:
+#     display_name: autojax
+#     language: python
+#     name: autojax
+# ---
+
+# %%
+from __future__ import annotations
+
+import jax
+import jax.numpy as jnp
+import numpy as np
+
+# %%
+N = 10
+
+# %%
+jax.config.update("jax_enable_x64", True)
+jax.config.update("jax_num_cpu_devices", N)
+
+# %%
+assert jax.device_count() == N
+
+
+# %%
+@jax.jit
+def f(
+    m: np.ndarray[tuple[int], np.float64],
+    k1: np.ndarray[tuple[int], np.float64],
+    k2: np.ndarray[tuple[int], np.float64],
+) -> np.ndarray[tuple[int], np.float64]:
+    """
+    memory used: MK
+    FLOPS: 4MK
+    """
+    A_mk = jnp.square(jnp.outer(m, k1))
+    return A_mk @ k2
+
+
+# %%
+M = 8000
+K = 8000
+
+# %%
+m = jnp.arange(1, M + 1, dtype=np.float64)
+k1 = jnp.arange(1, K + 1, dtype=np.float64)
+k2 = jnp.square(jnp.reciprocal(k1))
+
+# %%
+jax.debug.visualize_array_sharding(k1)
+
+# %%
+# %%timeit
+f(m, k1, k2).block_until_ready()
+
+# %%
+mesh = jax.make_mesh((N,), ("x",))
+mesh
+
+# %%
+from jax.sharding import PartitionSpec as P
+
+sharding = jax.sharding.NamedSharding(mesh, P("x"))
+
+# %%
+sharding
+
+# %%
+k1_sharded = jax.device_put(k1, sharding)
+k2_sharded = jax.device_put(k2, sharding)
+
+# %%
+jax.debug.visualize_array_sharding(k1_sharded)
+
+# %%
+# %%timeit
+f(m, k1_sharded, k2_sharded).block_until_ready()
